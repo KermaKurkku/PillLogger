d7040c6f45bf524e35d4e46b51087aa628f42555
Kermakurkku <jere.salmensaari@gmail.com>
2019-09-16 21:47:40 +0300

    Added basic layout Added TODOs Added switching to HistoryActivity

--- app/src/main/java/com/example/hyteprojekti/MainActivity.java
+++ app/src/main/java/com/example/hyteprojekti/MainActivity.java
@@ -2,13 +2,29 @@ package com.example.hyteprojekti;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+
+    //TODO get camera working
+    //TODO get notifications working
+
+    //TODO styling
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    //Changes the view of the app to showHistoryActivity when button is pressed
+    public void showHistory(View view) {
+        //Creates the intent to change the view
+        Intent changeView = new Intent(this, showHistoryActivity.class);
+        //Changes the view
+        startActivity(changeView);
+    }
+
 }
---
+++ app/src/main/java/com/example/hyteprojekti/showHistoryActivity.java
@@ -0,0 +1,18 @@
+package com.example.hyteprojekti;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class showHistoryActivity extends AppCompatActivity {
+
+    //TODO  all functionality
+    //TODO collect data
+    //TODO show collected data as list or graph
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_show_history);
+    }
+}
